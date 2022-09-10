6dae8840fd54c2573072a961a76f253fd4a631aa
Ринат Хамитов <stoneaz@mail.ru>
2022-09-10 17:54:45 +0300

    1 задание дз

---
+++ дз 1 задание/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace дз_1_задание
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Math.Exp(1));
+        }
+    }
+}
