b5ea3bb20a994b7424221d5be651b193c56a7fd9
Ринат Хамитов <stoneaz@mail.ru>
2022-09-10 18:33:05 +0300

    дз 5 задание

---
+++ дз 5 задание/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace дз_5_задание
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите сторну квадрата");
+            int square = int.Parse(Console.ReadLine());
+            Console.WriteLine($"периметр квадрата = {square * 4}");
+
+        }
+    }
+}
