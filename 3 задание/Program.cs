1f9914b27c19566c891260a3cf58ca991257044b
Ринат Хамитов <stoneaz@mail.ru>
2022-09-10 17:18:11 +0300

    испарвление

--- 3 задание/Program.cs
+++ 3 задание/Program.cs
@@ -9,11 +9,19 @@ namespace _3_задание
         {
             List<string> myList = new List<string>()
             {
-                "а", "б", "в", "г", "д","е","ё","ж", "з", "и", "й", "к","л","м", "н", "о", "п", "р", "с", "т", "у", "ф", "х", "ц", "ч", "ш", "щ", "ъ", "ы", "ь", "э","ю", "я"
+                "а", "б", "в", "г", "д","е","ё", "ж", "з", "и", "й", "к","л","м", "н", "о", "п", "р", "с", "т", "у", "ф", "х", "ц", "ч", "ш", "щ", "ъ", "ы", "ь", "э","ю", "я"
             };
             Console.Write("Введи букву: ");
             string str = Console.ReadLine();
-            int index = myList.FindIndex(a => a.Contains(str));
+            int index;
+            if (str == "я")
+            {
+                index = 0;
+            }
+            else
+            {
+                index = myList.FindIndex(a => a.Contains(str));
+            }
             Console.WriteLine($"Следующая буква: {myList[index + 1]}");
         }
     }
