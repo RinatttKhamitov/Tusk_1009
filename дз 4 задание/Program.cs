408b4dad73830f07b3f208e69682255aae9057ee
Ринат Хамитов <stoneaz@mail.ru>
2022-09-10 18:22:50 +0300

    дз 4 задание

---
+++ дз 4 задание/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace дз_4_задание
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine(num + 10);
+        }
+    }
+}
