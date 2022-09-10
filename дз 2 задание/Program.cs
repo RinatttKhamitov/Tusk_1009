803d390a1f3a227fc93ece802705ae052c9a4fa0
Ринат Хамитов <stoneaz@mail.ru>
2022-09-10 18:00:33 +0300

    дз 2 задание

---
+++ дз 2 задание/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace дз_2_задание
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("50");
+            Console.Write("10");
+        }
+    }
+}
