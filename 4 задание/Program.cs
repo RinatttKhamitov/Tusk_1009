39ab079ca3b576e093628f8c1c66fc1128490f3f
Ринат Хамитов <stoneaz@mail.ru>
2022-09-10 17:04:33 +0300

    4 задание в классе (через дискриминант)

---
+++ 4 задание/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _4_задание
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введи три числа коэффициенты уравнения");
+            Console.Write("число 1: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("число 2: ");
+            int b = int.Parse(Console.ReadLine());
+            Console.Write("число 3: ");
+            int c = int.Parse(Console.ReadLine());
+
+            double D = (b * b) - (4 * a * c);
+            D = Math.Sqrt(D);
+            double x1 = (-b + D) / 2 * a;
+            double x2 = (-b - D) / 2 * a;
+            Console.WriteLine($"первый корень {x1} второй корень {x2}");
+
+        }
+    }
+}
