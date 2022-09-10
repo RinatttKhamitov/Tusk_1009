9ef68fb47edbf1d03c28d51c874f2b162150fe9d
Ринат Хамитов <stoneaz@mail.ru>
2022-09-10 18:46:39 +0300

    дз 6 задание (длина окружности и площадь круга)

---
+++ дз 6 задание/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace дз_6_задание
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите радиус круга");
+            int radius = int.Parse(Console.ReadLine());
+            Console.WriteLine($"длина окружности = {2 * Math.PI * radius} и площадь круга = {Math.PI * Math.PI * radius}");
+        }
+    }
+}
