b45af9a6ebed205f6cbd677918ed5187540067ca
Ринат Хамитов <stoneaz@mail.ru>
2022-09-10 18:16:50 +0300

    дз 4 задание (Random)

---
+++ дз 3 задание/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace дз_3_задание
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < 4; i ++)
+            {
+                Console.WriteLine(rnd.Next());
+            }
+        }
+    }
+}
