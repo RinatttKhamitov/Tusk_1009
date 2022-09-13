65859c9ffc369c584b9ee10f4077040bad09cf55
Ринат Хамитов <stoneaz@mail.ru>
2022-09-13 21:12:22 +0300

    дз 10 задание (мир, труд, май)

---
+++ дз 10 задание/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace дз_10_задание
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mir = "Мир";
+            string tryd = "Труд";
+            string mai = "Май";
+            Console.WriteLine($"{mir} {tryd} {mai}");
+            Console.WriteLine(mir);
+            Console.WriteLine($"    {tryd}");
+            Console.WriteLine($"         {mai}");
+        }
+    }
+}
