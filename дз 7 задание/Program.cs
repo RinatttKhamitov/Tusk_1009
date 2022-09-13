32957b416395c08e8f1c89f70c0eb9fa219932d2
Ринат Хамитов <stoneaz@mail.ru>
2022-09-13 20:25:24 +0300

    дз 7 задание (y=cox(x))

---
+++ дз 7 задание/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace дз_7_задание
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Какой X?: ");
+            double angle = double.Parse(Console.ReadLine());
+            Console.WriteLine($"cos(x) = {Math.Cos(angle)}");
+        }
+    }
+}
