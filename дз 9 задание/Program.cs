948ce815f23dca93ec6c9bc36d2e5403612e1e02
Ринат Хамитов <stoneaz@mail.ru>
2022-09-13 20:56:25 +0300

    дз 9 задание (стоимость покупки)

---
+++ дз 9 задание/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace дз_9_задание
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Стоимость 1 кг конфет = 20 руб, печенья = 35 руб и яблок = 40 руб");
+            Console.Write("сколько купил кг конфет?: ");
+            int sweets = int.Parse(Console.ReadLine());
+            Console.Write("сколько купил кг печенья?: ");
+            int cookies = int.Parse(Console.ReadLine());
+            Console.Write("сколько купил кг яблок?: ");
+            int apples = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Стоимость покупки = {sweets * 20 + cookies * 35 + apples * 40}");
+        }
+    }
+}
