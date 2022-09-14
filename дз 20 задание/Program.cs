dd42fbd9404be77ceb5bdf20eee3508a4dcc780d
Ринат Хамитов <stoneaz@mail.ru>
2022-09-14 19:58:37 +0300

    дз 20 задание

---
+++ дз 20 задание/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace дз_20_задание
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"полных часов прошло с начала суток: {DateTime.Now.Hour}");
+            Console.WriteLine($"полных минут прошло с начала очередного часа: {DateTime.Now.Minute}");
+            Console.WriteLine($"полных секунд прошло с начала очередной минуты: {DateTime.Now.Second}");
+        }
+    }
+}
