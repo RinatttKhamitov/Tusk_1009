f5563bbae00519ad391bee882e9bde8a2105c80c
Ринат Хамитов <stoneaz@mail.ru>
2022-09-14 18:04:57 +0300

    дз 18 задание

---
+++ дз 18 задание/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace дз_18_задание
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введи x1 = ");
+            int x1 = int.Parse(Console.ReadLine());
+            Console.Write("Введи y1 = ");
+            int y1 = int.Parse(Console.ReadLine());
+            Console.Write("Введи x2 = ");
+            int x2 = int.Parse(Console.ReadLine());
+            Console.Write("Введи y2 = ");
+            int y2 = int.Parse(Console.ReadLine());
+            Console.WriteLine($"расстояния между ними ");
+        }
+    }
+}
