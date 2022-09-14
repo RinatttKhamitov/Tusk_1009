a7433bb417c3663499e2d6a4c87dac7a8862d1ed
Ринат Хамитов <stoneaz@mail.ru>
2022-09-14 17:23:13 +0300

    дз 13 задание

---
+++ дз 13 задание/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace дз_13_задание
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введи число");
+            string str = Console.ReadLine();
+            Console.WriteLine("Вы ввели число ");
+            Console.WriteLine(str);
+
+        }
+    }
+}
