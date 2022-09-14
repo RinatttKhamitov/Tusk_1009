35047b23a6fbd101ebbc10151be6d63dae4627f5
Ринат Хамитов <stoneaz@mail.ru>
2022-09-14 17:41:31 +0300

    дз 17 задание

---
+++ дз 17 задание/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace дз_17_задание
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Число 1 = ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Число 2 = ");
+            int a2 = int.Parse(Console.ReadLine());
+            Console.Write($"среднее арифметическое = {(a + a2)/2}, среднее геометрическое = {Math.Sqrt(a*a2)}");
+
+        }
+    }
+}
