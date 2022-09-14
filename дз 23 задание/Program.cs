d759bb1d393ed255bbd2877c1222c18cd07aebe6
Ринат Хамитов <stoneaz@mail.ru>
2022-09-14 20:48:45 +0300

    дз 23 задание

---
+++ дз 23 задание/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace дз_23_задание
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введи число n < 999: ");
+            string num = Console.ReadLine();
+            Console.WriteLine($"сотен: {num[num.Length - 3]}");
+            Console.WriteLine($"тысяч: {num[num.Length -4]}");
+
+        }
+    }
+}
