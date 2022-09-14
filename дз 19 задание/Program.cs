2698d58fb8e0e19ceee39ba8d81f5015142a3a21
Ринат Хамитов <stoneaz@mail.ru>
2022-09-14 19:51:16 +0300

    дз 19 задание

---
+++ дз 19 задание/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace дз_19_задание
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Выбери способ А или Б");
+            Console.Write("а = ");
+            var a = Console.ReadLine();
+            Console.Write("b = ");
+            var b = Console.ReadLine();
+            Console.Write("c = ");
+            var c = Console.ReadLine();
+            string ab = Console.ReadLine();
+            if (ab == "A" || ab == "А")
+            {
+                var d = b;
+                b = c;
+                c = a;
+                a = d;
+                Console.WriteLine($"a = {a}, b = {b}, c = {c}");
+            }
+            if (ab == "B" || ab == "В")
+            {
+                var d = b;
+                b = a;
+                a = c;
+                c = d;
+                Console.WriteLine($"a = {a}, b = {b}, c = {c}");
+            }
+        }
+    }
+}
