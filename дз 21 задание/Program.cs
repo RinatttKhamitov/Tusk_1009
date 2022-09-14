f750f24bda8d76a9cbc5800d64fb546b34798a12
Ринат Хамитов <stoneaz@mail.ru>
2022-09-14 20:08:43 +0300

    дз 21 задание

---
+++ дз 21 задание/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace дз_21_задание
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"из прямоугольника с размерами 543 х 130 мм можно отрезать {(543 - (543.0 % 130.0)) / 130} квадратов со стороной 130 мм ");
+        }
+    }
+}
