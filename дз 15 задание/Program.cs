4225103b1e4a264c8364a46acdc30349dfba3d88
Ринат Хамитов <stoneaz@mail.ru>
2022-09-14 17:32:04 +0300

    дз 15 задание

---
+++ дз 15 задание/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace дз_15_задание
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            Console.WriteLine($"1 число = {rnd.Next()}");
+            Console.WriteLine($"2 число = {rnd.Next()}");
+            Console.WriteLine($"3 число = {rnd.Next()}");
+            Console.WriteLine($"4 число = {rnd.Next()}");
+        }
+    }
+}
