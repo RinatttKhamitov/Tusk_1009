26943c35e4b3bd67d781a0b0a76c7b93d28cdad9
Ринат Хамитов <stoneaz@mail.ru>
2022-09-14 20:41:33 +0300

    дз 22 задание

---
+++ дз 22 задание/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace дз_22_задание
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введи трехзначное  число: ");
+            string num = Console.ReadLine();
+            num = (num[2] + String.Empty) + (num[1] + String.Empty) + (num[0] + String.Empty);
+            Console.Write(num);
+
+        }
+    }
+}
