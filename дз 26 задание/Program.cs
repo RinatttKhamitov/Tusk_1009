8b1ece1393d98458f0df82dbe2d8b4b26d2be715
Ринат Хамитов <stoneaz@mail.ru>
2022-09-15 19:53:19 +0300

    дз 26 задание

---
+++ дз 26 задание/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace дз_26_задание
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Сколько часов?");
+            int hours = int.Parse(Console.ReadLine());
+            Console.WriteLine("Сколько минут?");
+            int minutes = int.Parse(Console.ReadLine());
+            Console.WriteLine("Сколько секунд?");
+            int second = int.Parse(Console.ReadLine());
+            int time = (hours * 3600) + (minutes * 60) + second;
+            double angle = time * 2;
+            angle = angle * 360 / 86400;
+            if (angle > 360)
+            {
+                angle -= 360;
+            }
+            Console.WriteLine($"угол = {angle}");
+            
+
+        }
+    }
+}
