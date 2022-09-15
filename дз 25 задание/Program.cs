1ae0c07eeecf763929a62af3cf9460f9177b3650
Ринат Хамитов <stoneaz@mail.ru>
2022-09-15 19:18:17 +0300

    дз 25 задание

---
+++ дз 25 задание/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace дз_25_задание
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введи значение x = ");
+            string num = Console.ReadLine();
+            Console.WriteLine($"n = {num[1]}{num[0]}{num[1]}");
+
+        }
+    }
+}
