4e6a9e32194e65e4f76c4c7617e5eb8d8910e837
Ринат Хамитов <stoneaz@mail.ru>
2022-09-15 19:04:48 +0300

    дз 24 задание

---
+++ дз 24 задание/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace дз_24_задание
+{
+    class Program
+    {
+        public static string ReverseString(string s)
+        {
+            char[] arr = s.ToCharArray();
+            Array.Reverse(arr);
+            return new string(arr);
+
+        }
+            static void Main(string[] args)
+        {
+            Console.WriteLine("Введи четырехзначное число");
+            string num = Console.ReadLine();
+            string revnum = ReverseString(num);
+            Console.WriteLine(revnum);
+            num = (num[1] + String.Empty) + (num[0] + String.Empty) + (num[3] + String.Empty) + (num[2] + String.Empty);
+            while (num.Contains("0"))
+            {
+                if (num[0].ToString() == "0")
+                {
+                    num = num.Remove(0, 1);
+                    Console.WriteLine(num);
+                }
+                else
+                {
+                    break;
+                }
+
+            }
+            Console.WriteLine(num);
+
+        }
+    }
+}
