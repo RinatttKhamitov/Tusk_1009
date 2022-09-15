4a2d6e75d2c80482a1df3f2f0b195d5995d736ef
Ринат Хамитов <stoneaz@mail.ru>
2022-09-15 20:44:34 +0300

    дз 30 задание

---
+++ дз 30 задание/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace дз_30_задание
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введи число: ");
+            int num = int.Parse(Console.ReadLine());
+            int intData = (int)Math.Sqrt(num);
+            int howMuch = 0;
+            for (int i = 1; i < intData + 1 ; i++)
+            {
+                if (num % i == 0)
+                {
+                    howMuch++;
+                }
+            }
+            Console.WriteLine($"Число {num} имеет {howMuch + 1} делителей");
+        }
+    }
+}
