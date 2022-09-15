b07fd9e76b4455280449f54f0cc238e258c1c964
Ринат Хамитов <stoneaz@mail.ru>
2022-09-15 20:04:15 +0300

    дз 27 задание (не пон)

---
+++ дз 27 задание/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace дз_27_задание
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Сколько часов?");
+            int hours = int.Parse(Console.ReadLine());
+            Console.WriteLine("Сколько минут?");
+            int minutes = int.Parse(Console.ReadLine());
+        }
+    }
+}
