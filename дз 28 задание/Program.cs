614cf2308b2b65c273b0d6a21f2b4b135da0e2ee
Ринат Хамитов <stoneaz@mail.ru>
2022-09-15 20:12:40 +0300

    дз 28 задание

---
+++ дз 28 задание/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace дз_28_задание
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введи 3 числа");
+            int num = Math.Abs(int.Parse(Console.ReadLine()));
+            int num2 = Math.Abs(int.Parse(Console.ReadLine()));
+            int num3 = Math.Abs(int.Parse(Console.ReadLine()));
+            Console.WriteLine($"число меньшее по модулю = {Math.Min(num, Math.Min(num2, num3))}");
+        }
+    }
+}
