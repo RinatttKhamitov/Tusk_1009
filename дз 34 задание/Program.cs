2cf45bb4af940ed9fed5f34d5319fdc72c78ad0f
Ринат Хамитов <stoneaz@mail.ru>
2022-09-16 16:33:03 +0300

    дз 34 задание

---
+++ дз 34 задание/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace дз_34_задание
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Выбери а или б");
+            string ab = Console.ReadLine();
+            if (ab == "а" || ab == "А")
+            {
+                Console.Write("Как тебя завут?: ");
+                Console.WriteLine(Console.ReadLine());
+            }
+            if (ab == "б" || ab == "Б")
+            {
+                Console.Write("Как тебя завут?: ");
+                Console.WriteLine($"Привет, {Console.ReadLine()}");
+            }
+        }
+    }
+}
