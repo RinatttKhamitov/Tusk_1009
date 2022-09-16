949b08165b9827c6ecd4cdd8cced99857a416d3e
Ринат Хамитов <stoneaz@mail.ru>
2022-09-16 09:58:00 +0300

    дз 32 задание

---
+++ дз 32 задание/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace дз_32_задание
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введи первый член прогрессии");
+            int a1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введи второй член прогрессии");
+            int a2 = int.Parse(Console.ReadLine());
+            int d = a2 - a1;
+            Console.WriteLine("Введи номер прогрессии");
+            int n = int.Parse(Console.ReadLine());
+            int an = a1 + (n - 1) * d;
+            Console.WriteLine($"Ответ: {an}");
+
+        }
+    }
+}
